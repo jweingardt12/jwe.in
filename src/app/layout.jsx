bb8ca4f3cabf93c7f285877aae7f8825da45109f
Jason Weingardt <jweingardt12@gmail.com>
2025-04-01 11:34:20 -0400

    fix: move ssr:false dynamic imports to client component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from 'next-themes'
 import { Layout } from '../components/Layout'
 import { Toaster } from '../components/ui/toaster'
-import dynamic from 'next/dynamic'
 import { HighlightInit } from '@highlight-run/next/client'
 import { Toaster as SonnerToaster } from 'sonner'
 import { MDXWrapper } from '../components/MDXWrapper'
@@ -10,18 +9,15 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 import '@radix-ui/themes/styles.css'
 import mixpanel from '@/lib/mixpanel'
+import ClientSideImports from '@/components/ClientSideImports';
 
-const DotBackgroundDemo = dynamic(() => import('../components/ui/dot-background'), { ssr: false })
-const MixpanelTracker = dynamic(() => import('../components/MixpanelTracker'), { ssr: false })
-const OpenPanelWrapper = dynamic(() => import('../components/OpenPanelWrapper'), { ssr: false })
-
-export const viewport = {
+const viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
 }
 
-export const metadata = {
+const metadata = {
   title: {
     template: '%s - Jason Weingardt',
     default: 'Jason Weingardt - Product Manager, technologist, nerd.',
@@ -58,7 +54,7 @@ export default function RootLayout({ children }) {
             <MDXWrapper>
               <div className="relative flex w-full">
                 <div className="fixed inset-0">
-                  <DotBackgroundDemo />
+                  <ClientSideImports />
                 </div>
                 <div className="relative flex w-full flex-col">
                   <Layout>{children}</Layout>
@@ -68,8 +64,7 @@ export default function RootLayout({ children }) {
               <SonnerToaster position="bottom-right" theme="system" />
               <Analytics />
               <SpeedInsights />
-              <MixpanelTracker />
-              <OpenPanelWrapper />
+              <ClientSideImports />
             </MDXWrapper>
           </ThemeProvider>
         </body>
